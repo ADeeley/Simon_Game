23af7d5768beac32428809b88a9614d0f38231a8
Adam Deeley <ad.deeley@gmail.com>
2017-11-25 19:36:43 +0000

    Refactored the code to only reveal certain public methods using the revealing module pattern.

--- js/index.js
+++ js/index.js
@@ -10,171 +10,204 @@
  * @namespace Game
  * @class Game
  */
-const Game = {
-    colours: ['red', 'blue', 'yellow', 'green'],
-    pattern: [],
-    userPattern: [],
-    listening: false,
-    strict: false,
-    power: false,
-    colourButtons: document.getElementById('colourButtons'),
-    strictCheckbox: document.getElementById('strictToggle'),
-    start: document.getElementById('start'),
-    reset: document.getElementById('reset'),
-    powerButton: document.getElementById('power'),
-    alertText: document.getElementById('alertText')
-};
-/**
- * Sets up the function for play. Must be called first.
- *
- * @return null
- */
-Game.init = function() {
-    Game.uiBind();
-};
-/**
- * Serves to keep the DOM references separate from the logic.
- * 
- * @return null
- */
-Game.uiBind = function() {
-	Game.colourButtons.addEventListener('click', () => {
-		Game.getPlayerInput(event.target.id);
-	});
-	Game.start.addEventListener('click', () => {
-                console.log('Start');
-                Game.play();
-	});
-	Game.powerButton.addEventListener('click', () => {
-                Game.togglePower();
-	});
-	Game.strictCheckbox.addEventListener('click', () => {
-                Game.toggleStrictMode();
-	});
-};
-/**
- * Turns the game on and off
- *
- * @return null
- */
-Game.togglePower = function() {
-	Game.power = !Game.power;
-        console.log('Power: ' + Game.power);
-};
-/**
- * A switch to prevent the game from interpreting user clicks when not required
- * 
- * @return null
- */
-Game.toggleListenForPlayerInput = function() {
-    Game.listening = !Game.listening;
-    console.log('Listening: ' + Game.listening);
-};
-/**
- * Switches the game to the one-mistake-only version of the game.
- *
- * @return null
- */
-Game.toggleStrictMode = function() {
-    Game.strict = !Game.strict;
-};
-/**
- * Starts the game and plays the first pattern
- *
- * @return null
- */
-Game.play = function() {
-    if (!Game.power) {
-        console.log('Power not on.');
-        return;
+const Game = ( function() {
+    // members ===================================================
+    var colours = ['red', 'blue', 'yellow', 'green'],
+        pattern = [],
+        userPattern = [],
+        listening = false,
+        strict = false,
+        power = false,
+        alertText = document.getElementById('alertText');
+
+    // Privileged Methods ===================================================
+    /**
+     * Turns the game on and off
+     *
+     * @return null
+     */
+    function togglePower() {
+        power = !power;
+        console.log('Power: ' + power);
+        return this;
     }
-    Game.incrementPattern();
-    Game.playPattern();
-};
-/**
- * Animates the current colour in the sequence.
- *
- * @return null
- */
-Game.flash = function(colour) {
-    let el = document.getElementById(colour);
-    el.innerHTML = '!';
-    setTimeout(() => {
-        el.innerHTML = colour;
-    }, 1000);
-};
-/**
- * Displays a message to the user - e.g. 'Fail' and resets the alertMessage to '' after
- * 1 second.
- *
- * @param {String} message the string to display to the user
- * @return null
- */
-Game.displayAlert = function(message) {
-    Game.alertText.innerHTML = message;
-    setTimeout( () => {
-        Game.alertText.innerHTML = '';
-    }, 1000);
-};
-/**
- * Displays a message to the user - e.g. "Fail" and resets the alertMessage to "" after
- * 1 second.
- *
- * @param {String} message the string to display to the user
- * @return null
- */
-Game.displayAlert = function(message) {
-    Game.alertText.innerHTML = message;
-    setTimeout( () => {
-        Game.alertText.innerHTML = "";
-    }, 1000);
-};
-/**
- * Sets the Game object's parameters to their 'factory' settings.
- *
- * @return null
- */
-Game.reset = function() {
-    Game.pattern = [];
-    Game.userPattern = [];
-};
-/**
- * Adds another colour to the pattern that the user needs to copy
- *
- * @return null
- */
-Game.incrementPattern = function() {
-    let choice = Math.floor(Math.random() * 4);
-    Game.pattern.push(Game.colours[choice]);
-};
-/**
- * Runs through the current pattern recursively, animating each colour in sequence.
- *
- * @param {Number} i used to keep track of numbers remaining in the pattern array
- * @return null
- */
-Game.playPattern = function(i = 0) {
-    Game.flash(Game.pattern[i++]);
-    if (i < Game.pattern.length) {
-        return setTimeout(() => {
-            Game.playPattern(i);
+    /**
+     * Getter for power.
+     *
+     * @return {Boolean} The current power state
+     */
+    function isPoweredOn() {
+        return power;
+    }
+    /**
+     * A switch to prevent the game from interpreting user clicks when not required
+     * 
+     * @return null
+     */
+    function toggleListenForPlayerInput() {
+        listening = !listening;
+        console.log('Listening: ' + listening);
+        return this;
+    }
+    /**
+     * Getter method for listening.
+     * 
+     * @return {Boolean} If the game is ready for input. 
+     */
+    function isListening() {
+        // is this private?
+        return listening;
+    }
+    /**
+     * Switches the game to the one-mistake-only version of the game.
+     *
+     * @return null
+     */
+    function toggleStrictMode() {
+        strict = !strict;
+        return this;
+    }
+    /**
+     * Getter method for strict mode state.
+     * 
+     * @return {Boolean} If the game is in strict mode or not.
+     */
+    function isStrict() {
+        // is this private?
+        return strict;
+    }
+    /**
+     * Sets the Game object's parameters to their 'factory' settings.
+     *
+     * @return null
+     */
+    function reset() {
+        pattern = [];
+        userPattern = [];
+        return this;
+    }
+    /**
+     * Adds another colour to the pattern that the user needs to copy
+     *
+     * @return null
+     */
+    function incrementPattern() {
+        let choice = Math.floor(Math.random() * 4);
+        pattern.push(colours[choice]);
+        return this;
+    }
+    /**
+     * Resets the user input to a blank array.
+     *
+     * @return null
+     */
+    function clearUserPattern() {
+        userPattern = [];
+        return this;
+    }
+    /**
+     * Animates the current colour in the sequence.
+     *
+     * @return null
+     */
+    function flash(colour) {
+        let el = document.getElementById(colour);
+        el.innerHTML = '!';
+        setTimeout(() => {
+            el.innerHTML = colour;
         }, 1000);
-    } else Game.toggleListenForPlayerInput();
-    
-};
-/**
- * Checks if the pattern inputted by the player matches the generated pattern
- *
- * @return {Boolean} The decision as to if the strings match
- */
-Game.patternsMatch = function() {
-    for (let i = 0; i < Game.userPattern.length; i++) {
-        if (Game.userPattern[i] != Game.pattern[i]) {
-            return false;
-        }
     }
-    return true;
-};
+    /**
+     * Runs through the current pattern recursively, animating each colour in sequence.
+     *
+     * @param {Number} i used to keep track of numbers remaining in the pattern array
+     * @return null
+     */
+    function playPattern(i = 0) {
+        flash(pattern[i++]);
+        if (i < pattern.length) {
+            return setTimeout(() => {
+                playPattern(i);
+            }, 1000);
+        } else toggleListenForPlayerInput();
+        
+    }
+    /**
+     * Checks if the pattern inputted by the player matches the generated pattern
+     *
+     * @return {Boolean} The decision as to if the strings match
+     */
+    function patternsMatch() {
+        for (let i = 0; i < userPattern.length; i++) {
+            if (userPattern[i] != pattern[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * Displays a message to the user - e.g. "Fail" and resets the alertMessage to "" after
+     * 1 second.
+     *
+     * @param {String} message the string to display to the user
+     * @return null
+     */
+    function displayAlert(message) {
+        alertText.innerHTML = message;
+        setTimeout( () => {
+            alertText.innerHTML = '';
+        }, 1000);
+    }
+    /**
+     * Starts the game and plays the first pattern
+     *
+     * @return null
+     */
+    function play() {
+        if (!isPoweredOn()) {
+            console.log('Power not on.');
+            return;
+        }
+        incrementPattern();
+        console.log('Test');
+        playPattern();
+    }
+    /**
+     * Pushes the user input to the userPattern property
+     *
+     * @return null
+     */
+    function recordUserInput(colour) {
+        userPattern.push(colour);
+        return this;
+    }
+    /**
+     * Returns true if the userPattern and pattern are of the same length
+     *
+     * @return {Boolean} result of length test
+     */
+    function patternsAreOfEqualLength() {
+        return pattern.length === userPattern.length;
+    }
+    // Revealing module pattern ===========================================
+    return {
+        togglePower: togglePower,
+        isPoweredOn: isPoweredOn,
+        toggleListenForPlayerInput: toggleListenForPlayerInput,
+        toggleStrictMode: toggleStrictMode,
+        reset: reset,
+        incrementPattern: incrementPattern,
+        playPattern: playPattern,
+        patternsMatch: patternsMatch,
+        displayAlert: displayAlert,
+        isListening: isListening,
+        play: play,
+        recordUserInput: recordUserInput,
+        patternsAreOfEqualLength: patternsAreOfEqualLength,
+        isStrict: isStrict,
+        clearUserPattern: clearUserPattern
+    };
+})();
 /**
  * Adds the player input to the userPattern parameter and passes control to the process inputfunction
  *
@@ -184,11 +217,13 @@ Game.patternsMatch = function() {
 Game.getPlayerInput = function(colour) {
     // Prevent this function processing whilst other actions are 
     // being performed
-    if (!Game.listening) {
+    console.log('clicked ' + colour);
+
+    if (!Game.isListening) {
         return;
     }
 
-    Game.userPattern.push(colour);
+    Game.recordUserInput(colour);
     Game.processInput();
 };
 /**
@@ -198,15 +233,16 @@ Game.getPlayerInput = function(colour) {
  * @return null
  */
 Game.processInput = function() {
+    console.log('Processing Input...');
     if (!Game.patternsMatch()) {
-        if (Game.strict) {
+        if (Game.isStrict) {
             Game.displayAlert('Strict Failed');
             Game.reset();
             Game.toggleListenForPlayerInput();
             return;
         } else {
             Game.displayAlert('Try Again');
-            Game.userPattern = [];
+            Game.clearUserPattern();
             Game.toggleListenForPlayerInput();
             Game.playPattern();
             return;
@@ -214,15 +250,39 @@ Game.processInput = function() {
     }
 
     // Only proceed if the user has inputted a whole pattern
-    if (Game.userPattern.length != Game.pattern.length) {
+    if (!Game.patternsAreOfEqualLength()) {
+        console.log('not of equal len');
         return;
     } else {
         Game.displayAlert('Match');
         console.log('Match');
-        Game.userPattern = [];
+        Game.clearUserPattern();
         Game.toggleListenForPlayerInput();
         Game.incrementPattern();
         Game.playPattern();
     }
 };
-Game.init();
+
+// One-time DOM event listener inits 
+/**
+ * Serves to keep the DOM references separate from the logic.
+ * 
+ * @return null
+ * 
+ */
+document.getElementById('colourButtons').addEventListener('click', () => {
+    Game.getPlayerInput(event.target.id);
+});
+document.getElementById('start').addEventListener('click', () => {
+    console.log('Start');
+    Game.play();
+});
+document.getElementById('power').addEventListener('click', () => {
+    Game.togglePower();
+});
+document.getElementById('strictToggle').addEventListener('click', () => {
+    Game.toggleStrictMode();
+});
+document.getElementById('reset').addEventListener('click', () => {
+    Game.reset();
+});
